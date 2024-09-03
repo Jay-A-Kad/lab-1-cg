87cb21da73b1cc5418692a15ca07421a64a37fd0
Jay-A-Kad <jaykad982@gmail.com>
2024-09-03 09:34:03 -0400

    added lab files

---
+++ Lab1/stub.cpp
@@ -0,0 +1,25 @@
+//
+//		          Programming Assignment #1
+//
+//					Daljit Singh Dhillon
+//
+//
+/***************************************************************************/
+/* Include needed files */
+
+
+
+/***************************************************************************/
+// Forward declarations
+void drawPixel(int x, int y);
+
+void drawLineDDA(float xStart, float yStart, float xEnd, float yEnd);
+
+void drawLineDDA(float xStart, float yStart, float xEnd, float yEnd)
+{
+	drawPixel((int)xStart,(int)yStart);
+	drawPixel((int)xEnd, (int)yEnd);
+
+	//replace above two lines with your own inplementation
+}
+
